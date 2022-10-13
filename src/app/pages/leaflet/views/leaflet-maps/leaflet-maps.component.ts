53145f50144f39ef6b3546a17f72bd1d0443120b
Rodrigo Crespo <rodrigo.crespo@syloper.com>
2022-10-12 21:26:56 -0300

    featl: implement leaflet

--- src/app/pages/leaflet/views/leaflet-maps/leaflet-maps.component.ts
+++ src/app/pages/leaflet/views/leaflet-maps/leaflet-maps.component.ts
@@ -1,14 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-leaflet-maps',
   templateUrl: './leaflet-maps.component.html',
   styleUrls: ['./leaflet-maps.component.scss'],
 })
-export class LeafletMapsComponent implements OnInit {
+export class LeafletMapsComponent {
+  map: L.Map;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {}
+  ionViewDidEnter() {
+    this.leafletMap();
+  }
 
+  leafletMap() {
+    // Para poder cargar el mapa, necesitamos indicarle a leaflet el identificador
+    // Le indicaremos a leaflet que utilice el elemento con id mapId.
+    this.map = L.map('mapId').setView([-32.94682, -60.63932], 5);
+
+    // A continuación, tenemos que añadirle un «tileLayer» a nuestro mapa.
+    // Un «tile» es una imagen que representa un área determinada.
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(
+      this.map
+    );
+
+    // Añadir marcadores a nuestro mapa
+    L.marker([-32.94682, -60.63932]).bindPopup('Pop up').addTo(this.map);
+  }
 }
